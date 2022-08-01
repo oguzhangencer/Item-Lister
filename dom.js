2f499b7ef94504d64cd2e85517eb472fada804b6
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-01 18:47:52 +0300

    get_elem_cls_name

---
+++ dom.js
@@ -0,0 +1,48 @@
+//EXAMINE THE DOCUMENT OBJECT //
+
+/*
+console.dir(document)
+console.log(document)
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+document.title = 123
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.scripts)
+console.log(document.all[10])
+document.all[10].textContent = 'HI'
+console.log(document.forms)
+console.log(document.links)
+*/
+
+// GET ELEMENT ID //
+/*
+console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title')
+var header = document.getElementById('main-header')
+console.log(headerTitle)
+
+headerTitle.textContent = 'Hello'
+headerTitle.innerHTML = 'Goodbye'
+console.log(headerTitle.textContent)
+headerTitle.innerHTML = '<h3>Hello</h3>'
+
+headerTitle.style.borderBottom = 'solid 4px #098'
+*/
+
+// GET ELEMENT CLASS NAME //
+var items = document.getElementsByClassName('list-group-item')
+console.log(items)
+console.log(items[0])
+items[0].textContent = 'Hola'
+items[0].style.fontWeight = 'bold'
+items[1].style.backgroundColor = 'lightgreen'
+items[0].style.backgroundColor = 'lightyellow'
+
+//Gives error
+//items.style.backgroundColor = '#f4f4f4'
+
+for(var i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4'
+}
