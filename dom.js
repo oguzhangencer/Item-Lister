223e2b9686fcd531472f4bbea992f8fc80a61b6f
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-01 22:26:53 +0300

    query_selector

--- dom.js
+++ dom.js
@@ -64,20 +64,38 @@ for(var i = 0; i < li.length; i++){
 }
 */
 
+//QUERY SELECTOR //
+/* 
+var header = document.querySelector('#main-header')
+header.style.borderBottom = 'solid 4px #ccc'
 
+var input = document.querySelector('input')
+input.value = ('Input a value')
 
+var submit = document.querySelector('input[type = "submit"]')
+submit.value = 'SEND'
 
+var item = document.querySelector('.list-group-item')
+item.style.color = 'red'
 
+var lastItem = document.querySelector('.list-group-item:last-child')
+lastItem.style.color = 'blue'
 
+var secondItem = document.querySelector('.list-group-item:nth-child(2')
+secondItem.style.color = 'coral'
+*/
+// QUERY SELECTOR ALL //
+/*  
+var titles = document.querySelectorAll('.title')
 
+console.log(titles)
+titles[0].textContent = 'Hello'
 
+var odd = document.querySelectorAll('li:nth-child(odd)')
+var even = document.querySelectorAll('li:nth-child(even)')
 
-
-
-
-
-
-
-
-
-
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4'
+    even[i].style.backgroundColor = '#ccc'
+}
+*/
