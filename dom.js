8d54c1dbc046bb47255d29fc8a89ef06a72cadb2
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-01 22:45:45 +0300

    first&last_child

--- dom.js
+++ dom.js
@@ -99,3 +99,44 @@ for(var i = 0; i < odd.length; i++){
     even[i].style.backgroundColor = '#ccc'
 }
 */
+
+
+// TRAVERSING THE DOM //
+
+var itemList = document.querySelector('#items')
+/*  
+// parentNode
+console.log(itemList.parentNode)
+itemList.parentNode.style.backgroundColor = '#f4f4f4'
+console.log(itemList.parentNode.parentNode.parentNode)
+
+// parentElement
+console.log(itemList.parentElement)
+itemList.parentElement.style.backgroundColor = '#f4f4f4'
+console.log(itemList.parentElement.parentElement.parentElement)
+
+
+// childNodes
+console.log(itemList.childNodes)
+
+
+console.log(itemList.children)
+console.log(itemList.children[1])
+itemList.children[1].style.backgroundColor = 'yellow'
+
+// firstChild
+console.log(itemList.firstChild)
+// firstElementChild
+console.log(itemList.firstElementChild)
+itemList.firstElementChild.textContent = 'Hi Guys!'
+
+// lastChild
+console.log(itemList.lastChild)
+// lastElementChild
+console.log(itemList.lastElementChild)
+itemList.lastElementChild.textContent = 'Hey Guys!'
+*/
+
+
+
+
